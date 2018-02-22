efc5b0e2dfe8e07abd5764245b8e8479ff19af08
BlackKnightBR <blackknight@blackknightstudio.com.br>
2018-02-22 16:40:44 -0300

    Adição de arquivos media, entertaiment_center

---
+++ entertaiment_center.py
@@ -0,0 +1,12 @@
+import media
+import fresh_tomatoes.py
+
+movies = [
+
+	toy_story = media.Movies("Toy Story", "https://img.fstatic.com/p3ICCo1m3Vo2YVgG1ownzV0f-38=/fit-in/290x478/smart/https://cdn.fstatic.com/media/movies/covers/2014/06/toy-story_t1407.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc&t=5s"),
+
+	transformers = media.Movies("", "", "")
+
+]
+
+open_movies_page(movies)
