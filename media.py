00f4a3a4c0b239e361fd69b7b4f08976771452db
BlackKnightBR <blackknight@blackknightstudio.com.br>
2018-02-24 10:26:31 -0300

    Arrumando código.

--- media.py
+++ media.py
@@ -1,19 +1,18 @@
 class Movies():
-	""" Class that defines a movie. """
+    """ Class that defines a movie. """
 
-# Function that intanciates the class Movies,
-# using as parameter, title, art(poster)
-# and url youtube trailer
+    # Function that intanciates the class Movies,
+    # using as parameter, title, art(poster)
+    # and url youtube trailer
 
-
-def __init__(self, movie_title, poster_image, trailer_youtube):
-    """
-        Inicializes intances of Movies class,
-        :param movie_title: string
-        :param poster_image: string
-        :param trailer_youtube: string
-    """
-self.title = movie_title
-self.poster_image_url = poster_image
-self.trailer_youtube_url = trailer_youtube
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        """
+            Inicializes intances of Movies class,
+            :param movie_title: string
+            :param poster_image: string
+            :param trailer_youtube: string
+        """
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
