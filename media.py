e19f63462f45dac42e99ee57d4021f9442160c83
BlackKnightBR <blackknight@blackknightstudio.com.br>
2018-02-26 18:29:11 -0300

    Atualização de arquivos segundo PEP8

--- media.py
+++ media.py
@@ -1,9 +1,17 @@
+##########################################
+# Project: Movie Website
+# Date Started: 22/02/2018
+# Date Completed: 26/02/2018
+# Submitted by: Rodrigo Montebello Saboya Brito
+##########################################
+
+######################################## Media File ####################################################
+# Description: This file creates the class Movie to allow for instances of this class to be used in the
+#              entertainment.py file. This definition of the class Movie was obtained through the course
+########################################################################################################
+
 class Movies():
     """ Class that defines a movie. """
-
-    # Function that intanciates the class Movies,
-    # using as parameter, title, art(poster)
-    # and url youtube trailer
 
     def __init__(self, movie_title, poster_image, trailer_youtube):
         """
